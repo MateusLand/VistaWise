8fd2aca89d39e4813c638a4288f4588a138ecc3b
MateusLand <mateusland@gmail.com>
2024-03-11 13:34:29 +0000

    glide added for better image rendering

--- app/src/main/java/com/example/vistawise/model/Destination.kt
+++ app/src/main/java/com/example/vistawise/model/Destination.kt
@@ -2,5 +2,5 @@ package com.example.vistawise.model
 
 data class Destination(
     val name: String,
-    val imageResId: Int // Resource ID of the destination image
+    val imageResId: String // Resource ID of the destination image
 )
--- app/src/main/java/com/example/vistawise/network/DestinationService.kt
+++ app/src/main/java/com/example/vistawise/network/DestinationService.kt
@@ -1,25 +1,21 @@
 package com.example.vistawise.network
 
-import com.example.vistawise.R
 import com.example.vistawise.model.Destination
 
 class DestinationService {
 
     suspend fun retrieveTopDestinations(): Result<List<Destination>> {
         return try {
-            val destinations = listOf(
-                Destination("London", R.drawable.london),
-                Destination("Paris", R.drawable.paris),
-                Destination("Budapest", R.drawable.budapest),
-                Destination("Rome", R.drawable.rome)
+            val mainDestinationUrls = listOf(
+                Destination("London", "https://media.cntraveler.com/photos/6362cedd30f7bfad82fb4d8e/master/w_1600,c_limit/big%20ben-GettyImages-1388339818.jpeg"),
+                Destination("Paris", "https://c0.wallpaperflare.com/preview/73/406/408/architecture-city-travel-sky.jpg"),
+                Destination("Budapest", "https://i.pinimg.com/736x/7b/30/03/7b30037132197c4940452608c07e728b.jpg"),
+                Destination("Rome", "https://a.storyblok.com/f/53624/1365x2040/cea2c909f6/young-womans-standing-in-front-of-the-colosseum.jpg")
                 // Add more destinations as needed
             )
-            Result.success(destinations)
+            Result.success(mainDestinationUrls)
         } catch (e: Exception) {
             Result.failure(e)
         }
     }
 }
-
-
-// add links URL to images
